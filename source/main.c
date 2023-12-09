1b044dc392571d665af1178322663ee360e69d55
VocalFan <45863583+VocalFan@users.noreply.github.com>
2023-12-08 22:46:01 -0500

    Fixes and better documentation (#1)

--- source/main.c
+++ source/main.c
@@ -36,108 +36,114 @@ static int get_auth(struct thread *td, struct get_auth_args *args) {
 }
 
 int _main(struct thread *td) {
-	int fd;
-	struct stat st;
-	char *buf, *next;
-	size_t i;
-	struct get_auth_req req;
+    int fd, write_result;
+    struct stat st;
+    char *buf, *next;
+    size_t i;
+    struct get_auth_req req;
 
-	initKernel();
-	initLibc();
-	jailbreak();
-	mmap_patch();
-	initSysUtil();
+    initKernel();
+    initLibc();
+    jailbreak();
+    mmap_patch();
+    initSysUtil();
 
-	// open list file
-	fd = open("/mnt/usb0/dump-list.txt", O_RDONLY, 0);
+    // open list file
+    fd = open("/mnt/usb0/dump-list.txt", O_RDONLY, 0);
+    if (fd < 0) {
+        printf_notification("Cannot open dump-list.txt on the USB drive.");
+        return 0;
+    }
 
-	if (fd < 0) {
-		printf_notification("Cannot open dump-list.txt on the USB drive.");
-		return 0;
-	}
+    if (fstat(fd, &st) < 0) {
+        printf_notification("Cannot determine the size of dump-list.txt on the USB drive.");
+        close(fd);
+        return 0;
+    }
 
-	if (fstat(fd, &st) < 0) {
-		printf_notification("Cannot determine the size of dump-list.txt on the USB drive.");
-		return 0;
-	}
+    // read list file
+    buf = malloc(st.st_size + 1);
+    if (!buf) {
+        printf_notification("Failed to allocate memory.");
+        close(fd);
+        return 0;
+    }
 
-	// read list file
-	buf = malloc(st.st_size + 1);
-	i = 0;
+    i = 0;
+    while (i < (size_t)st.st_size) {
+        ssize_t n = read(fd, buf + i, st.st_size - i);
+        if (n < 0) {
+            printf_notification("Cannot read dump-list.txt on the USB drive.");
+            free(buf);
+            close(fd);
+            return 0;
+        } else if (n == 0) {
+            break;
+        }
+        i += n;
+    }
 
-	while (i != st.st_size) {
-		ssize_t n = read(fd, buf + i, st.st_size - i);
+    buf[st.st_size] = '\0';
+    close(fd);
 
-		if (n < 0) {
-			printf_notification("Cannot read dump-list.txt on the USB drive.");
-			return 0;
-		}
+    // parse list file
+    next = buf;
+    for (;;) {
+        char *p;
 
-		i += n;
-	}
+        if (!*next) {
+            break;
+        }
 
-	buf[st.st_size] = 0;
-	close(fd);
+        p = strchr(next, ':');
+        if (!p) {
+            printf_notification("dump-list.txt on the USB drive has invalid format.");
+            free(buf);
+            return 0;
+        }
+        *p = '\0';
 
-	// parse list file
-	next = buf;
+        req.file = next;
+        if (kexec(get_auth, &req) < 0) {
+            printf_notification("Failed to dump auth info for\n%s", next);
+            free(buf);
+            return 0;
+        }
 
-	for (;;) {
-		char *p;
+        next = p + 1;
+        p = strchr(next, '\n');
+        if (p) {
+            if (p[-1] == '\r') {
+                p[-1] = '\0';
+            }
+            *p = '\0';
+        }
 
-		// check if no more entries
-		if (!*next) {
-			break;
-		}
+        fd = open(next, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        if (fd < 0) {
+            printf_notification("Cannot create\n%s", next);
+            free(buf);
+            return 0;
+        }
 
-		// get target file
-		if (!(p = index(next, ':'))) {
-			printf_notification("dump-list.txt on the USB drive has invalid format.");
-			return 0;
-		}
+        write_result = write(fd, req.buf, 136);
+        if (write_result < 0 || write_result != 136) {
+            printf_notification("Failed to write auth info.");
+            close(fd);
+            free(buf);
+            return 0;
+        }
+        close(fd);
 
-		*p = 0;
+        if (!p) { // if there's no newline, end the loop
+            break;
+        }
 
-		// get auth info
-		req.file = next;
+        next = p + 1;
+    }
 
-		if (kexec(get_auth, &req) < 0) {
-			printf_notification("Failed to dump auth info for %s.", next);
-			return 0;
-		}
+    free(buf);
+    printf_notification("Auth info dump complete!");
 
-		// get output file
-		next = p + 1;
-
-		if (p = index(next, '\n')) {
-			*p = 0;
-
-			if (p[-1] == '\r') {
-				p[-1] = 0;
-			}
-		}
-
-		// write to usb drive
-		fd = open(next, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-
-		if (fd < 0) {
-			printf_notification("Cannot create %s.", next);
-			return 0;
-		}
-
-		write(fd, req.buf, 136);
-		close(fd);
-
-		// move to next line
-		if (!p) {
-			break;
-		}
-
-		next = p + 1;
-	}
-
-	free(buf);
-	printf_notification("Auth info dump completed!");
-
-	return 0;
+    return 0;
 }
